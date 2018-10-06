fd72a9bf40b813d6c3afac68e6790b241666bbbe
Mike Hurd <mhurd@users.noreply.github.com>
2018-10-06 12:45:44 +0100

    Initial commit

---
+++ one-dimensional-2-states-CA.py
@@ -0,0 +1,28 @@
+# 1D 2-STATES CELLULAR AUTOMATON
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+N = 513         # Size of the 1D CA
+n_iter = 512    # Number of iterations
+
+image = np.zeros((n_iter, N), dtype=np.int)             # Initialise the lattice to white
+image[0, round(N/2)] = 1                                # But set the central node to black
+
+# Iteration loop (each line)
+for iterate in range(1, n_iter):
+    # Lattice loop (along the line)
+    for j in range(1, N-1):
+        # Third CA rule from book
+        if image[iterate-1, j+1] + image[iterate-1, j-1] == 1:
+            # Turn node black
+            image[iterate, j] = 1
+        # Enforce periodicity (but don't fall off the end of the matrix)
+        if iterate < n_iter-1:
+            # Move the end value to the start of the next iteration
+            image[iterate+1, 0] = image[iterate, N-2]
+            # Move the last value to the start of the next row
+            image[iterate+1, N-1] = image[iterate, 1]
+
+plt.imshow(image, interpolation="nearest")
+plt.show()
