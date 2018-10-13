d9317b941e5522ddf39ea6e424d6a528304fbe62
Mike Hurd <mhurd@users.noreply.github.com>
2018-10-13 17:01:06 +0100

    Changed marker size and made lattice bigger

--- two-dimensional-2-states-CA-on-triangular-lattice.py
+++ two-dimensional-2-states-CA-on-triangular-lattice.py
@@ -3,15 +3,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-N = 24              # Size of the 2D CA
-n_iter = 10         # Number of iterations
-n_neighbour = 6     # Number of connected neighbours per node
+N = 64                  # Size of the 2D CA
+n_iter = 30             # Number of iterations
+n_neighbour = 6         # Number of connected neighbours per node
+scatter_marker = "."    # Plot marker style
+marker_style = dict(color='black', linestyle=':', marker='o',
+                    markersize=3, markerfacecoloralt='gray')
 
 dx = np.array([1, 0, 1, -1, 0, -1])     # nearest neighbour template x
 dy = np.array([-1, -1, 0, 1, 1, 0])     # nearest neighbour template y
 image = np.zeros((N, N), dtype=np.int)  # Initialise the lattice to white
 image[N//2, N//2] = 1                   # But set the central node to black
-plt.scatter(N//2, N//2)
+plt.plot(N//2, N//2, **marker_style)
 plt.axis([0, N, 0, N])
 plt.gca().set_aspect(1)
 
@@ -30,7 +33,7 @@ for iterate in range(1, n_iter):
             if image[i, j] == 0 and acc == 1:
                 update[i, j] = 1
                 # Plot the node (triangular lattice)
-                plt.scatter(j+(i-N//2)/2., N//2+0.866*(i-N//2))
+                plt.plot(j+(i-N//2)/2., N//2+0.866*(i-N//2), **marker_style)
     # End lattice loops
     # synchronous update of CA
     image += update
